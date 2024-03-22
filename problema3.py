3642eb782f0c7e0ade53635f3968752bd6d90b94
Miel <m.ferrada09@ufromail.cl>
2024-03-22 10:07:49 -0300

    implementada solución a problema 3 :D

--- problema3.py
+++ problema3.py
@@ -9,5 +9,27 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def arbolTernario(numero):
+	return [numero, [], [], []]
+
+def insertaEnArbolTernario(arbol,numero):
+	if arbol==[]:
+		arbol+=arbolTernario(numero)
+	elif numero < arbol[0]:
+		insertaEnArbolTernario(arbol[1],numero)
+	elif numero == arbol[0]:
+		insertaEnArbolTernario(arbol[2],numero)
+	else:
+		insertaEnArbolTernario(arbol[3],numero)
+
+t = input().split()
+try:
+	for num in range(len(t)):
+		t[num]=int(t[num])
+	arbolito=arbolTernario(t[0])
+	for num in t[1:-1]:
+		insertaEnArbolTernario(arbolito,num)
+	print(arbolito)
+except ValueError:
+	print()
+
